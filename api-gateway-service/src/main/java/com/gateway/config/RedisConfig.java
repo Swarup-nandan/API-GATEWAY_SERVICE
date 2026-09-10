9de84d9084001fedc0bc7948b3c2b749ef04f28a
Swarup Nandan Muduli <swarupnandanmuduli@gmail.com>
2026-09-10 21:47:13 +0530

    Format RedisConfig.java for consistency

--- api-gateway-service/src/main/java/com/gateway/config/RedisConfig.java
+++ api-gateway-service/src/main/java/com/gateway/config/RedisConfig.java
@@ -2,14 +2,14 @@ package com.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory; 
+import org.springframework.data.redis.core.StringRedisTemplate; 
 
 @Configuration
-public class RedisConfig {
-
+public class RedisConfig { 
+    
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {   
+        return new StringRedisTemplate(connectionFactory);  
     }
-}
+} 
